da2a86b2374043a9160f9c02d79ec9cf843d2bad
yusifhadi12 <64900783+yusifhadi12@users.noreply.github.com>
2020-07-17 18:45:13 +0700

    Add files via upload
    php

---
+++ index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+<title>The Priemer Hotel</title>
+<link rel="icon" href="logo.png">
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+<link href="style.css" type="text/css" rel="stylesheet" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<script src="holder.js"></script>
+ </head>
+ <body>	
+
+<div id="container">
+<!-- header -->
+	<header>
+	<center>
+		<img src="1.PNG" style="width: 100%; height: 100%;"><br>
+		<div style="width: 300px">
+	
+		</div>
+	</center>
+	</header>
+  
+
+
+<!-- content -->
+<hr style="border-bottom: 20px solid white">
+ <div id="wrapper">
+<div id="main"> 
+
+            <div class="row"> 
+                <div class="box"> 
+                   <img src="2.JPG"> <img src="3.JPG"> 
+                   <div class="border">
+                    <p></p>
+                     <hr style="border-bottom: 20px solid black">
+                      
+                    </div>
+                </div> 
+                </div>
+            </div>
+            <div class="row"> 
+                <div class="box"> 
+               <img src="4.JPG">  <img src="5.jpeg">
+              <div class="border">
+                <p></p>
+                     <hr style="border-bottom: 20px solid black">
+              </div>
+                </div> 
+                 <div class="box"> 
+               <img src="6.JPG">  <img src="7.JPG"> 
+               <div class="border">
+                    <p></p>
+                     <hr style="border-bottom: 20px solid black">
+                  
+                </div>
+                </div> 
+            </div> 
+
+
+<section role="slider" class="slide-panel">
+  <font color="black"> 
+	<h1><u><center>The Priemer Hotel </center></u></h1>
+	<p>Menetapkan standar baru untuk industri perhotelan di Pekanbaru, kami menawarkan lokasi baru bagi tamu kami untuk mengalami janji merek Grand Zuri bahwa "kami tahu bagaimana cara menyenangkan Anda". Total kamar terdiri dari 168 kamar dan suite,</p>
+  <br>
+   <hr style="border-bottom: 20px solid black">
+   <p></p>
+<h1><b><u><p>LAYANAN HOTEL</p></u></b></h1>
+<br>
+<p>Layanan binatu & dry cleaning</p>
+<p>Lantai bebas rokok</p>
+<p>Akses internet nirkabel</p>
+<p>Layanan kamar 24 jam</p>
+<p>Operator 24 jam</p>
+<p>Semua kartu kredit utama diterima</p>
+<p>Layanan antar-jemput ke / dari bandara Sultan Syarif Kasim II</p>
+<p>Staf hotel dua bahasa</p>
+<p>Pusat bisnis</p>
+<p>Toko Hadiah</p>
+<p>Mushola tamu</p>
+<p>11 ruang pertemuan / fungsi dengan 2 area pra fungsi yang luas</p>
+<p>450 Banyak gedung parkir mobil</p>
+<p>Karaoke dengan 10 kamar pribadi</p>
+<br>
+<br> 
+
+<h1><b><u><p>FASILITAS KAMAR</p></u></b></h1>
+<br>
+<p>Fasilitas membuat teh & kopi gratis</p>
+<p>Akses internet nirkabel</p>
+<p>Gratis 2 botol air minum</p>
+<p>Koran nasional gratis</p>
+<p>Penyejuk udara yang dikontrol secara individual</p>
+<p>Bak mandi atau pancuran individu dengan air panas & dingin</p>
+<p>Tirai hitam</p>
+<p>Sistem kartu kunci elektronik</p>
+<p>Telepon SLI menyatukan di kamar tidur dan kamar mandi</p>
+<p>Mini bar</p>
+<p>Brankas</p>
+<p>Televisi LCD multi saluran satelit</p>
+<p>Perlengkapan mandi lengkap dan fasilitasnya termasuk pengering rambut, sandal, dan jubah mandi *</p>
+<p>Zuri Dream - tempat tidur berkualitas tinggi untuk tidur nyenyak - tidur oleh King Koil®</p>
+<br>
+<br>
+<h1><b><u><p>168 KAMAR & SUITES</p></u></b></h1>
+
+Hotel ini memiliki 168 kamar & suite. Semua kamar berdesain modern, memadukan kesederhanaan dengan kenyamanan dan kemudahan luar biasa. Semua suite memiliki ruang tamu yang terpisah, meja kerja yang luas, kamar mandi besar dengan bathtub, dan dilengkapi dengan Wi-Fi, unit telepon baik di kamar dan kamar mandi dengan akses SLI, kabel TV layar datar 32 dan 42 inci, dan mini bar.
+ <br>
+ <br>
+<h1><b><u><p>GAMBARAN</p></u></b></h1>
+
+Spa dan pusat kebugaran oleh De Vata. Terletak di lantai 5 di dekat kolam hidro, adalah bar kolam renang yang menyajikan minuman sehat, mulai dari jus segar hingga teh herbal. Fasilitas ini juga mencakup kolam renang, sauna, dan kolam pusaran air dengan area relaksasi.
+<br>
+<br>
+<h1><b><u><p>11 RUANG PERTEMUAN</p></u></b></h1>
+
+Hotel ini menawarkan 1 pusat bisnis, 11 ruang pertemuan, dan sebuah ballroom, The Premiere Ballroom. menampung antara 20 hingga 2500 tamu untuk berbagai kesempatan.
+ <br>
+ <br>
+<h1><b><u><p>THE CAFE</p></u></b></h1>
+
+24 jam. Masuki restoran kafe kosmopolitan kami di lantai lobi dan nikmati suasananya yang berkembang. Di mana Anda dapat menikmati santapan santai sepanjang hari dalam lingkungan yang terang alami atau bersantap secara eksklusif dengan tamu Anda di malam hari. Menyajikan beragam hidangan, mulai dari hidangan lokal hingga internasional, semua disiapkan dan dipimpin oleh Kepala Chef kami; pemenang 5 kali dari kompetisi memasak televisi nasional dengan pengalaman luas dari seluruh dunia.
+ <br>
+<br>
+<h1><b><u><p>PREMIERE LOUNGE</p></u></b></h1>
+
+Terletak di lantai 2, menghadap pemandangan Jalan Jenderal Sudirman yang indah, di sinilah Anda akan menemukan lounge kontemporer yang ditata elegan dengan gudang anggur dan ruang cerutu, yang dibuat untuk kerumunan orang Pekanbaru yang canggih. Nikmati suasana, isi ulang setelah hari yang sibuk, atau bahkan hanya bersantai dengan hiburan musik live kami. Lounge pribadi untuk mereka yang lebih menyukai suasana intim dengan sekelompok teman juga tersedia.
+ <br>
+ <br>
+<h1><b><u><p>LOUNGE CLUB EKSEKUTIF</p></u></b></h1>
+
+Siap secara eksklusif untuk mereka yang tinggal di lantai eksekutif kami adalah lounge klub pribadi ini, yang terletak di lantai 12. Sarapan, waktu minum teh, atau makanan ringan akan disajikan sepanjang hari untuk kesenangan dan kenyamanan tamu lantai eksekutif kami.
+ 
+
+ </section>
+ <hr style="border-bottom: 20px solid black">
+   
+ 
+
+            
+           
+          <p>CONTACT US</p>
+<p>Jl. Jenderal Sudirman No. 389</p>
+<p>28116</p>
+<p>Pekanbaru, Riau</p>
+<p>(+62) 761 7891818</p>
+<p>(+62) 761 7891919</p>
+
+
+			
+	<!-- footer -->
+	<footer>
+      
+   
+    <p><b>LOKASI</b></p>
+kami terletak di jantung kota, hanya 15 menit dari Bandara Internasional Sultan Syarif Kasim II dan berlokasi strategis di antara gedung-gedung pemerintahan dan gedung-gedung perkantoran besar.
+<br>
+<br>
+		<p>&copy;Copyright(C) since 2015. All rights reserved.</p>
+<p>The Premiere PekanbaruZuri Hotel Management</p>
+	</footer>
+</div>  
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">THE PREMIERE</a>
+    </div>
+  </div>
+</nav>
+  
+<div class="container">
+  <div align="center">
+    <h3><b>Pencarian dan Pagination PHP & MYSQL</b></h3>
+    <h4><b>THE PREMIERE HOTEL</b></h4>
+  </div>
+  <!--Panel Form pencarian -->
+  <div class="row">
+    <div class="col-md-5">
+      <div class="panel panel-default">
+        <div class="panel-heading"><b>Search</b></div>
+        <div class="panel-body">
+          <form class="form-inline" >
+            <div class="form-group">
+              <select class="form-control" id="Kolom" name="Kolom" required="">
+                <?php
+                  $kolom=(isset($_GET['Kolom']))? $_GET['Kolom'] : "";
+                ?>
+                <option value="Type_Kamar" <?php if ($kolom=="Type_Kamar") echo "selected"; ?>>Tipe_Kamar</option>
+                <option value="Jenis_Kamar" <?php if ($kolom=="Jenis_Kamar") echo "selected";?>>Jenis_Kamar</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" id="KataKunci" name="KataKunci" placeholder="KataKunci" required="" value="<?php if (isset($_GET['KataKunci']))  echo $_GET['KataKunci']; ?>">
+            </div>
+            <button type="submit" class="btn btn-primary">Search</button>
+            <a href="index.php" class="btn btn-danger">Delete</a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Tabel data Hotel -->
+  <table class="table table-striped table-bordered table-hover">
+    <thead>
+      <tr>
+        <th>No Kamar</th>
+        <th>Tipe Kamar</th>
+        <th>Jenis Kamar</th>
+        <th>Harga</th>
+      </tr>
+    </thead>  
+    <tbody>
+      <?php
+      include "koneksi.php";
+     
+      $page = (isset($_GET['page']))? (int) $_GET['page'] : 1;
+      
+      $kolomCari=(isset($_GET['Kolom']))? $_GET['Kolom'] : "";
+     
+      $kolomKataKunci=(isset($_GET['KataKunci']))? $_GET['KataKunci'] : "";
+
+      // Jumlah data per halaman
+      $limit = 5;
+
+      $limitStart = ($page - 1) * $limit;
+      
+      //kondisi jika parameter pencarian kosong
+      if($kolomCari=="" && $kolomKataKunci==""){
+        $SqlQuery = mysqli_query($con, "SELECT * FROM room LIMIT ".$limitStart.",".$limit);
+      }else{
+        //kondisi jika parameter kolom pencarian diisi
+        $SqlQuery = mysqli_query($con, "SELECT * FROM room WHERE $kolomCari LIKE '%$kolomKataKunci%' LIMIT ".$limitStart.",".$limit);
+      }
+      
+      $no = $limitStart + 1;
+      while($row = mysqli_fetch_array($SqlQuery)){ 
+      ?>
+        <tr>
+          <td><?php echo $row['No_Kamar']; ?></td>
+          <td><?php echo $row['Tipe_Kamar']; ?></td>
+          <td><?php echo $row['Jenis_Kamar']; ?></td>
+          <td><?php echo $row['Harga'] ?></td>
+        </tr>
+      <?php           
+      }
+      ?>
+    </tbody>      
+  </table>
+  <div align="right">
+    <ul class="pagination">
+      <?php
+        // Jika page = 1, maka LinkPrev disable
+        if($page == 1){ 
+      ?>        
+        <!-- link Previous Page disable --> 
+        <li class="disabled"><a href="#">Previous</a></li>
+      <?php
+        }
+        else{ 
+          $LinkPrev = ($page > 1)? $page - 1 : 1;  
+
+          if($kolomCari=="" && $kolomKataKunci==""){
+          ?>
+            <li><a href="index.php?page=<?php echo $LinkPrev; ?>">Previous</a></li>
+       <?php     
+          }else{
+        ?> 
+          <li><a href="index.php?Kolom=<?php echo $kolomCari;?>&KataKunci=<?php echo $kolomKataKunci;?>&page=<?php echo $LinkPrev;?>">Previous</a></li>
+         <?php
+           } 
+        }
+      ?>
+
+      <?php
+        //kondisi jika parameter pencarian kosong
+        if($kolomCari=="" && $kolomKataKunci==""){
+          $SqlQuery = mysqli_query($con, "SELECT * FROM Room");
+        }else{
+          //kondisi jika parameter kolom pencarian diisi
+          $SqlQuery = mysqli_query($con, "SELECT * FROM Room WHERE $kolomCari LIKE '%$kolomKataKunci%'");
+        }     
+      
+        //Hitung semua jumlah data yang berada pada tabel Sisawa
+        $JumlahData = mysqli_num_rows($SqlQuery);
+        
+        // Hitung jumlah halaman yang tersedia
+        $jumlahPage = ceil($JumlahData / $limit); 
+        
+        // Jumlah link number 
+        $jumlahNumber = 1; 
+
+        // Untuk awal link number
+        $startNumber = ($page > $jumlahNumber)? $page - $jumlahNumber : 1; 
+        
+        // Untuk akhir link number
+        $endNumber = ($page < ($jumlahPage - $jumlahNumber))? $page + $jumlahNumber : $jumlahPage; 
+        
+        for($i = $startNumber; $i <= $endNumber; $i++){
+          $linkActive = ($page == $i)? ' class="active"' : '';
+
+          if($kolomCari=="" && $kolomKataKunci==""){
+      ?>
+          <li<?php echo $linkActive; ?>><a href="index.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+
+      <?php
+        }else{
+          ?>
+          <li<?php echo $linkActive; ?>><a href="index.php?Kolom=<?php echo $kolomCari;?>&KataKunci=<?php echo $kolomKataKunci;?>&page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+          <?php
+        }
+      }
+      ?>
+      
+      <!-- link Next Page -->
+      <?php       
+       if($page == $jumlahPage){ 
+      ?>
+        <li class="disabled"><a href="#">Next</a></li>
+      <?php
+      }
+      else{
+        $linkNext = ($page < $jumlahPage)? $page + 1 : $jumlahPage;
+       if($kolomCari=="" && $kolomKataKunci==""){
+          ?>
+            <li><a href="index.php?page=<?php echo $linkNext; ?>">Next</a></li>
+       <?php     
+          }else{
+        ?> 
+           <li><a href="index.php?Kolom=<?php echo $kolomCari;?>&KataKunci=<?php echo $kolomKataKunci;?>&page=<?php echo $linkNext; ?>">Next</a></li>
+      <?php
+        }
+      }
+      ?>
+    </ul>
+  </div>
+</div>
+</body>
+</html>
+
